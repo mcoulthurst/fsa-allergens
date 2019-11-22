649f302ddd157435030e4b6d7907ed5e62dcb5d9
mcoulthurst <mc@lightness.co.uk>
2019-11-22 11:50:40 +0000

    Add a simple form, check and confirmation pages

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # fsa-allergens
-GDS Prototype for design prototypes
+The foundation for design prototypes for Report an Allergen using the GDS Prototype kit.
 
 ## Prototype Kit
-
 Install and run as per guide
 https://govuk-prototype-kit.herokuapp.com/docs
 
 
+The kit follows the Tutorial
+http://localhost:3000/docs/tutorials-and-examples
--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,23 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+var months = [null, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+router.get('/check-your-answers', function(req, res, next) {
+  //console.log(req);
+  console.log(months)
+  console.log(req.session.data['incident-month']);
+  var mon = parseInt(req.session.data['incident-month'])
+  var monthName = months[mon];
+  console.log(monthName);
+  res.render('check-your-answers', {
+                monthName: monthName
+              });
+
+});
+
+
+
 module.exports = router
